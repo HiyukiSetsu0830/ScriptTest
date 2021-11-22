efb70274bad9ffc9647c5ec204daf68a36bcbca9
ykimura0830 <ykimura_work0830@gmail.com>
2021-11-22 14:21:56 +0900

    スクリプト修正
    課題スクリプトを発展課題で上書きしてしまったため、修正。

--- Assets/Scripts/Lesson4_task.cs
+++ Assets/Scripts/Lesson4_task.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class Lesson4_task : MonoBehaviour {
 
-    
+    int[] array = {1,2,3,4,5};
     void Start() {
 
-        Boss boss = new Boss();
-        for (int i = 0; i <= 10; i++) {
-            boss.Magic();
+        for (int i = 0; i < array.Length; i++) {
+            Debug.Log(array[i]);
         }
-        
-    }
-}
 
-public class Boss {
-
-    int mp = 53;
-
-    public void Magic() {
-        if (mp >= 5) {
-            mp -= 5;
-            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
-
-        } else {
-            Debug.Log("MPが足りないため、魔法が使えない。");
+        for (int i = array.Length - 1; i >= 0; i--) {
+            Debug.Log(array[i]);
         }
-    }
 
+    }
 }
--- Assets/Scripts/Lesson4_task2.cs
+++ Assets/Scripts/Lesson4_task2.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lesson4_task2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Lesson4_task2 : MonoBehaviour {
+
+
+    void Start() {
+
+        Boss boss = new Boss();
+        for (int i = 0; i < 10; i++) {
+            boss.Magic();
+        }
+
+        boss.Magic();
+
+    }
+}
+
+public class Boss {
+
+    int mp = 53;
+
+    public void Magic() {
+        if (mp >= 5) {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+
+        } else {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
